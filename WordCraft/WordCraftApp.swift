0fe88dbd4d55996f4d42ef187836ef4508a96914
Daniel Nielsen <daniel@aplacebetweenhere.net>
2024-08-24 21:03:00 +0200

    Initial Commit

---
+++ WordCraft/WordCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCraftApp.swift
+//  WordCraft
+//
+//  Created by Daniel Nielsen on 24/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
